cada1bf301f80b3f5ff04b7e3befa94c808a2939
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-12-09 15:43:31 +0300

    Created starting conditions

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
 
 // Selectors
 const score0El = document.getElementById('score--0'),
-  score1El = document.getElementById('score--1');
+  score1El = document.getElementById('score--1'),
+  diceEl = document.querySelector('.dice');
 
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
