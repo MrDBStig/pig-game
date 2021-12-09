c02ca04a21dfdee3309d925e9c16589c1e4b074d
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-12-09 15:39:12 +0300

    Created project files

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Variables
+
+// Selectors
+const score0El = document.getElementById('score--0'),
+  score1El = document.getElementById('score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
